a28565a12bb1887f0524161e4e75995960a35b69
Abhishek Babu <164913157+TheBizarreAbhishek@users.noreply.github.com>
2026-06-28 19:32:51 +0530

    anykernel: Add dual M35+A35 DTBO detection for Bizarre Kernel

--- anykernel.sh
+++ anykernel.sh
@@ -1,10 +1,11 @@
 ### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+## Bizarre Kernel for Galaxy M35 (m35x) + Galaxy A35 (a35x)
+## Exynos 1380 (s5e8835) | One UI 7 / Android 16
+## by TheBizarreAbhishek
 
 ### AnyKernel setup
-# global properties
 properties() { '
-kernel.string= BizarreKernel Kernel || by Abhishek Babu
+kernel.string=Bizarre Kernel by TheBizarreAbhishek
 do.devicecheck=0
 do.soccheck=1
 do.modules=0
@@ -25,17 +26,55 @@ ramdisk_compression=auto
 patch_vbmeta_flag=auto
 NO_MAGISK_CHECK=1
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh
+# import functions/variables and setup patching (DO NOT REMOVE)
+. tools/ak3-core.sh;
 
 # boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ] || [ -L "/dev/block/bootdevice/by-name/init_boot" -o -L "/dev/block/by-name/init_boot" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ] \
+  || [ -L "/dev/block/bootdevice/by-name/init_boot" -o -L "/dev/block/by-name/init_boot" ]; then
+    split_boot;
+    flash_boot;
 else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+    dump_boot;
+    write_boot;
 fi
 ## end boot install
 
-flash_generic vendor_boot;
+# Flash DTBO based on detected device
+device_codename=$(getprop ro.product.device);
+device_model=$(getprop ro.product.model);
+
+ui_print " ";
+ui_print "  Bizarre Kernel | Exynos 1380 (s5e8835)";
+ui_print "  Device: $device_model ($device_codename)";
+ui_print " ";
+
+case "$device_codename" in
+  m35x)
+    ui_print "- Flashing Galaxy M35 DTBO...";
+    flash_generic dtbo-m35x.img;
+    ;;
+  a35x)
+    ui_print "- Flashing Galaxy A35 DTBO...";
+    flash_generic dtbo-a35x.img;
+    ;;
+  *)
+    case "$device_model" in
+      *M356B*|*M35*)
+        ui_print "- Flashing Galaxy M35 DTBO (model match)...";
+        flash_generic dtbo-m35x.img;
+        ;;
+      *A356B*|*A35*)
+        ui_print "- Flashing Galaxy A35 DTBO (model match)...";
+        flash_generic dtbo-a35x.img;
+        ;;
+      *)
+        ui_print "! Unknown device: $device_model ($device_codename)";
+        ui_print "! Skipping DTBO flash.";
+        ;;
+    esac
+    ;;
+esac
+
+ui_print " ";
+ui_print "  Done! Reboot to enjoy Bizarre Kernel.";

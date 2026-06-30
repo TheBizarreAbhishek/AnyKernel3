c3a618172f779e0b1a8360c3410089b328194268
Abhishek Babu <thebizarreabhishek@noreply.github.com>
2026-06-30 18:35:36 +0530

    fix: Add custom interactive logs and disable DTB/DTBO flashing

--- anykernel.sh
+++ anykernel.sh
@@ -1,96 +1,56 @@
 ### AnyKernel3 Ramdisk Mod Script
-## Bizarre Kernel for Galaxy M35 (m35x) + Galaxy A35 (a35x)
-## Exynos 1380 (s5e8835) | One UI 7 / Android 16
+## Bizarre Kernel for Galaxy M35/A35 (s5e8835)
 ## by TheBizarreAbhishek
 
 ### AnyKernel setup
 properties() { '
 kernel.string=Bizarre Kernel by TheBizarreAbhishek
 do.devicecheck=0
-do.soccheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.soc=s5e8835
-supported.versions=13 - 17
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
 ### AnyKernel install
-# boot shell variables
+## boot shell variables
 block=boot
 is_slot_device=auto
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
-NO_MAGISK_CHECK=1
+no_magisk_check=1
 
 # import functions/variables and setup patching (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh
 
-# boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ] \
-  || [ -L "/dev/block/bootdevice/by-name/init_boot" -o -L "/dev/block/by-name/init_boot" ]; then
-    split_boot;
-    flash_boot;
+ui_print " "
+ui_print "****************************************"
+ui_print "*      Bizarre Kernel | s5e8835        *"
+ui_print "*      by Abhishek Babu                *"
+ui_print "****************************************"
+ui_print " "
+
+ui_print "- Preparing installation environment..."
+split_boot
+
+if [ -f "split_img/ramdisk.cpio" ]; then
+    ui_print "- Unpacking ramdisk..."
+    unpack_ramdisk
+    ui_print "- Writing new kernel Image..."
+    write_boot
 else
-    dump_boot;
-    write_boot;
+    ui_print "- Flashing kernel Image..."
+    flash_boot
 fi
-## end boot install
 
-# Flash DTBO based on detected device
-device_codename=$(getprop ro.product.device);
-device_model=$(getprop ro.product.model);
-
-ui_print " ";
-ui_print "  Bizarre Kernel | Exynos 1380 (s5e8835)";
-ui_print "  Device: $device_model ($device_codename)";
-ui_print " ";
-
-case "$device_codename" in
-  m35x)
-    ui_print "- Flashing Galaxy M35 DTBO...";
-    mv dtbo-m35x.img dtbo.img
-    flash_generic dtbo;
-    ui_print "- Flashing Exynos 1380 DTB...";
-    mv dtb dtb.img
-    flash_generic dtb;
-    ;;
-  a35x)
-    ui_print "- Flashing Galaxy A35 DTBO...";
-    mv dtbo-a35x.img dtbo.img
-    flash_generic dtbo;
-    ui_print "- Flashing Exynos 1380 DTB...";
-    mv dtb dtb.img
-    flash_generic dtb;
-    ;;
-  *)
-    case "$device_model" in
-      *M356B*|*M35*)
-        ui_print "- Flashing Galaxy M35 DTBO (model match)...";
-        mv dtbo-m35x.img dtbo.img
-        flash_generic dtbo;
-        ui_print "- Flashing Exynos 1380 DTB...";
-        mv dtb dtb.img
-        flash_generic dtb;
-        ;;
-      *A356B*|*A35*)
-        ui_print "- Flashing Galaxy A35 DTBO (model match)...";
-        mv dtbo-a35x.img dtbo.img
-        flash_generic dtbo;
-        ui_print "- Flashing Exynos 1380 DTB...";
-        mv dtb dtb.img
-        flash_generic dtb;
-        ;;
-      *)
-        ui_print "! Unknown device: $device_model ($device_codename)";
-        ui_print "! Skipping DTBO/DTB flash.";
-        ;;
-    esac
-    ;;
-esac
-
-ui_print " ";
-ui_print "  Done! Reboot to enjoy Bizarre Kernel.";
+ui_print " "
+ui_print "Done! Reboot to enjoy Bizarre Kernel!"
+ui_print " "
